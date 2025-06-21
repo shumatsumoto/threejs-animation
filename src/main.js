aaed5758806bdbf47c35ff1e26be1245f50fde0d
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-06-22 06:58:34 +0900

    オブジェクト追加

--- src/main.js
+++ src/main.js
@@ -33,6 +33,20 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(window.devicePixelRatio);
 
+//オブジェクト作成
+const boxGeometry = new THREE.BoxGeometry(5, 5, 5, 10);
+const boxMaterial = new THREE.MeshNormalMaterial();
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.set(0, 0.5, -15);
+box.rotation.set(1, 1, 0);
+
+const torusGeometry = new THREE.TorusGeometry(8, 2, 16, 100);
+const torusMaterial = new THREE.MeshNormalMaterial();
+const torus = new THREE.Mesh(torusGeometry, torusMaterial);
+torus.position.set(0, 1, 10);
+
+scene.add(box, torus);
+
 //アニメーション
 const tick = () => {
   window.requestAnimationFrame(tick);
